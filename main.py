e08faaaeb783cb8f112cddd7eaf25c535486f387
Nour Hamdan <nour.ham96@gmail.com>
2024-08-11 15:28:38 +0300

    Create columns, image and description.

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Nour Hamdan")
+    content = """
+    Hi, I am Nour! I am an electrical engineer, programmer and artist. I graduated in 2020 with a bachelor degree in electrical engineer from Al-balqa'a applie university in Jordan.
+"""
+    st.info(content)
